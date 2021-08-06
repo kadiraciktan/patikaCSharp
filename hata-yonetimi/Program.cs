8dce61b94adcda2757ec877ea9e197ee3ae44a70
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-06 18:33:47 +0300

    Hata Yönetimi  - Exception Handling

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı  : " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata : " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+            }
+
+
+
+            try
+            {
+                // int a = int.Parse("test");
+                // int a = int.Parse(null);
+                int a = int.Parse("111111111111");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Değer çok küçük yada çok büyük");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
