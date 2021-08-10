2ce6227c1841844ec462a447a1b5ab84bcde2330
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-10 19:31:19 +0300

    Enumlar

---
+++ enumlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace enumlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if(sicaklik <=(int)HavaDurumu.Normal){
+                Console.WriteLine("Havanın biraz daha ısınmasını bekleyebilirsiniz");
+            }else if(sicaklik>=(int)HavaDurumu.Sıcak){
+                Console.WriteLine("Dışarıya Çıkmak İçin Çok Sıcak");
+            }else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSıcak) {
+                Console.WriteLine("Hava dışarı çıkmaya uygun");
+            }
+
+            Console.Read();
+        }
+    }
+
+    enum Gunler{
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu{
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
