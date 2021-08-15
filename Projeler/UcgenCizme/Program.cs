3bc59aa6ac9c147763e6c426e1b2e72fc1d3e96c
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 09:39:32 +0300

    Proje Eklendi

---
+++ Projeler/UcgenCizme/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Sayı Giriniz : ");
+Ucgen.Ciz(int.Parse(Console.ReadLine()));
+
+
+
+
+static class Ucgen
+{
+    public static void Ciz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+            for (int l = i - 1; l >= 1; l--)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+        }
+    }
+}
