9ea3049f3c5465f096cef668f234dc4c0e2f6dc8
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 09:47:47 +0300

    Proje Eklendi

---
+++ Projeler/daireCizme/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Sayı Giriniz : ");
+Daire.Ciz(int.Parse(Console.ReadLine()));
+
+public static class Daire
+{
+
+    public static void Ciz(int x)
+    {
+        int i, j;
+        int N = x;
+        for (i = 1; i <= N; i++)
+        {
+            for (j = 1; j <= N; j++)
+            {
+                if ((i == 1 || i == N) && (j == 1 || j == N))
+                {
+                    Console.Write(" ");
+                }
+                else if (i == 1 || i == N || j == 1 || j == N)
+                {
+
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
