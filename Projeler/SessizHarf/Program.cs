cd853fb636d040048ee41b1f5c11018bd2ba3d0f
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 12:59:49 +0300

    Proje Eklendi

---
+++ Projeler/SessizHarf/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Metin Giriniz : ");
+string metin = Console.ReadLine().ToUpper();
+
+char[] sesliler = new char[] { 'A','E','I','İ','O','Ö','U','Ü' };
+
+
+int sessizSayi = 0;
+foreach (var item in metin)
+{
+    if (!sesliler.Contains(item))
+    {
+        sessizSayi++;
+    }
+    else if (sessizSayi==2)
+    {
+        Console.WriteLine(true);
+    }
+    else
+    {
+        sessizSayi--;
+        if (sessizSayi<=0)
+        {
+            sessizSayi = 0;
+        }
+    }
+}
+if (sessizSayi<2)
+{
+    Console.WriteLine(false);
+}
+
+Console.Read();
