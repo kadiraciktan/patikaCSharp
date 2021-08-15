06fbd8fcb590505494d4707b03783c6830b607f2
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 10:02:34 +0300

    Proje Eklendi

---
+++ Projeler/Algoritma/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Yazı Giriniz : ");
+string[] yazi = Console.ReadLine().Split(',');
+
+if (yazi.Length==2)
+{
+    string metin = yazi[0];
+    int.TryParse(yazi[1],out int sira);
+    if (sira<=metin.Length && sira>0)
+    {
+        metin = metin.Remove(sira,1);
+       
+    }
+    Console.WriteLine("Sonuc : {0}", metin);
+}
