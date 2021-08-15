d62c807c9b0b29887942e2af7aed8b55f0005c56
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 09:24:40 +0300

    Proje Eklendi

---
+++ Projeler/ortalamaHesaplama/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Sayı Giriniz : ");
+int sayi = int.Parse(Console.ReadLine());
+float sayi1 = 0;
+float sayi2 = 1;
+float sayi3 = 0;
+for (int i = 0; i < sayi; i++)
+{
+    sayi3 = sayi1 + sayi2;
+    sayi1 = sayi2;
+    sayi2 = sayi3;
+}
+
+Console.WriteLine("Girdiğiniz Sayının Fibonacci Ortalaması : {0}", sayi3 / sayi);
