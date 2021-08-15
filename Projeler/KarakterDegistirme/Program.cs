99a38652b64243899a47e559693e658f93b16ca2
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 12:49:00 +0300

    Proje Eklendi

---
+++ Projeler/KarakterDegistirme/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Metin Giriniz : ");
+
+string metin = Console.ReadLine();
+
+char[] dizi = metin.ToCharArray();  
+
+char temp = dizi[0];    
+
+char last =  dizi[dizi.Length - 1];
+
+dizi[0] = last;
+dizi[dizi.Length - 1] = temp;
+
+foreach (var item in dizi)
+{
+    Console.Write(item);
+}
+
+Console.Read();
