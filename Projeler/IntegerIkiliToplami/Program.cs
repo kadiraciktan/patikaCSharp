2d00a8b4ed797d93891c9710eb95464f46b262b2
Kadir AÇIKTAN <kadiraciktan@gmail.com>
2021-08-15 12:14:39 +0300

    Proje Eklendi

---
+++ Projeler/IntegerIkiliToplami/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Kaç Adet Sayı İkilisi Gireceksiniz ?  :");
+int count  = int.Parse(Console.ReadLine());
+string[] ikililer = new string[count];
+for (int i = 0; i < count; i++)
+{
+    ikililer[i] = Console.ReadLine(); 
+}
+
+foreach (var item in ikililer)
+{
+    int sayi1 = int.Parse(item.Split(' ')[0]);
+    int sayi2 = int.Parse(item.Split(' ')[1]);
+    if (sayi1!=sayi2)
+    {
+        Console.WriteLine(sayi1 + sayi2);
+    }
+    else
+    {
+        Console.WriteLine(sayi1 * sayi1);
+    }
+}
+
+Console.Read();
+
